c5dddd407a0753413a37640fc3319cbb6e8714d7
AndyGeek <andy.eulogio@gmail.com>
2020-10-17 15:14:03 -0500

    Add register component

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const RegisterScreen = () => {
   return (
-    <div>
-      <h1>Register Screen</h1>
-    </div>
+    <>
+      <h3 className="auth__title">Register</h3>
+      <form>
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="Name"
+          name="name"
+          autoComplete="off"
+        />
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="Email"
+          name="email"
+          autoComplete="off"
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Password"
+          name="password"
+          autoComplete="off"
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Confirm"
+          name="password2"
+          autoComplete="off"
+        />
+        <button className="btn btn-primary" type="submit">
+          Register
+        </button>
+        <div className="mt-1">
+          <Link to="/auth/login" className="link" >
+            Already register?
+          </Link>
+        </div>
+      </form>
+    </>
   );
 };
