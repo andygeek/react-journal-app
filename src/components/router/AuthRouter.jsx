f8ddf340c293ce8f28408adf027cded6c9208804
AndyGeek <andy.eulogio@gmail.com>
2020-10-17 11:55:31 -0500

    Finish routes

--- src/components/router/AuthRouter.jsx
+++ src/components/router/AuthRouter.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { LoginScreen } from "../auth/LoginScreen";
+import { RegisterScreen } from "../auth/RegisterScreen";
 
 export const AuthRouter = () => {
   return (
     <div>
-      <h1>Auth Router</h1>
+      <Switch>
+        <Route path="/auth/login" exact component={LoginScreen} />
+        <Route path="/auth/register" exact component={RegisterScreen} />
+        <Redirect to="/auth/register" />
+      </Switch>
     </div>
   );
 };
